33b1acf23b7871ba4bc4d521d864b05d57ded306
Merel Jacobs <merel.burleigh@gmail.com>
2023-07-11 09:38:18 +0800

    working on test files

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ class SVGcreate{
         if (shape == "circle") {
             svgFile = `<svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg"><g><circle cx="100" cy="100" r="98" stroke="${this.shapeColor}" fill="${this.shapeColor}" stroke-width="5"/><text x="0" y="50" font-family="Verdana" font-size="35" fill="${this.textColor}">${this.text}</text></g></svg>`;
         } else if (shape == "square") {
-            svgFile = `<svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg"><g><rect x="10" y="10" width="30" height="30" stroke="${this.shapeColor}" fill="${this.shapeColor}" stroke-width="5"/><text x="0" y="50" font-family="Verdana" font-size="35" fill="${this.textColor}">${this.text}</text></g></svg>`;
+            svgFile = `<svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg"><g><rect x="10" y="10" width="200" height="200" stroke="${this.shapeColor}" fill="${this.shapeColor}" stroke-width="5"/><text x="55" y="120" font-family="Verdana" font-size="70" fill="${this.textColor}">${this.text}</text></g></svg>`;
         } else if (shape == "triangle") {
             svgFile = `<svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
             <g><polygon points="50 15, 100 100, 0 100" stroke="${this.shapeColor}" fill="${this.shapeColor}" stroke-width="5"/>
